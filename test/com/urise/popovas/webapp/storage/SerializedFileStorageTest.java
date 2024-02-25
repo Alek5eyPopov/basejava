72435e6dcfc92b0948e2ac050ae17a49ab0ed6af
Alek5eyPopov <aleksej_popov_88@mail.ru>
2024-02-25 20:37:22 +0300

    Домашнее задание №9

--- test/com/urise/popovas/webapp/storage/SerializedFileStorageTest.java
+++ test/com/urise/popovas/webapp/storage/SerializedFileStorageTest.java
@@ -1,7 +1,11 @@
 package com.urise.popovas.webapp.storage;
 
+import com.urise.popovas.webapp.storage.Serializer.ObjectSerializer;
+
+import java.io.File;
+
 public class SerializedFileStorageTest extends AbstractStorageTest{
     public SerializedFileStorageTest() {
-        super(new SerializedFileStorage());
+        super(new SerializedFileStorage(new ObjectSerializer(), new File("D:\\ResumeArchive")));
     }
 }
