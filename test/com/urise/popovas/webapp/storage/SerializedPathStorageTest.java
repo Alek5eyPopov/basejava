40b0ad7a8a18792ab63faf988e8b5b3d684fb327
Alek5eyPopov <aleksej_popov_88@mail.ru>
2024-03-02 09:54:16 +0300

    Домашнее задание №9 v2

--- test/com/urise/popovas/webapp/storage/SerializedPathStorageTest.java
+++
@@ -1,9 +0,0 @@
-package com.urise.popovas.webapp.storage;
-
-import com.urise.popovas.webapp.storage.Serializer.ObjectSerializer;
-
-public class SerializedPathStorageTest extends AbstractStorageTest{
-    public SerializedPathStorageTest() {
-        super(new SerializedPathStorage(new ObjectSerializer(), "D:\\ResumeArchive"));
-    }
-}
